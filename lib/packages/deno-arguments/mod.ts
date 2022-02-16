d1d398b8c9f3755c069063463125e5fd82b19764
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-16 15:01:58 +0100

    Remove local packages

--- lib/packages/deno-arguments/mod.ts
+++
@@ -1,4 +0,0 @@
-export { Arguments } from "./lib/Arguments.ts"
-export { ArgumentException } from "./lib/ArgumentException.ts";
-export { HelpException } from "./lib/HelpException.ts";
-export { ValueException } from "./lib/ValueException.ts";
