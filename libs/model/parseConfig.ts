9fb05d563220234ccb1b6c1a7237886b15642b39
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-21 18:04:21 +0100

    Update parseConfig.ts

--- libs/model/parseConfig.ts
+++ libs/model/parseConfig.ts
@@ -3,15 +3,28 @@
  */
 
 
-import { join, basename, isAbsolute } from "https://deno.land/std@0.126.0/path/mod.ts";
-import { ConfigSchema } from "../types/ConfigSchema.ts";
-import { ConfigType } from "../types/ConfigType.ts";
+import { join, basename } from "https://deno.land/std@0.126.0/path/mod.ts";
+import { type ConfigSchema, type VariablesType } from "../types/ConfigSchema.ts";
+import { type ConfigType } from "../types/ConfigType.ts";
+import { makeAbsolute } from "../helpers/makeAbsolute.ts";
+import * as styles from "../helpers/styles.ts";
 
 
-export function parseConfig(json: string, destinationRoot: string, separateGitRoot: string): ConfigType {
+
+const regex = {
+    variable: /\$\{(?<name>[A-Z_][A-Z0-9_]*)(\|(?<filter>.+?))?\}/g,
+}
+
+
+export function parseConfig(json: string, configRoot: string, separateGitRoot: string): ConfigType {
     const config: ConfigType = [];
 
     const data = JSON.parse(json) as ConfigSchema;
+
+    const variables = parseVariables(configRoot, data.variables ?? {});
+    console.log(variables);
+
+    const getVariable = createVariableGetter(variables);
 
     const packages = data.packages ?? {};
 
@@ -26,14 +39,11 @@ export function parseConfig(json: string, destinationRoot: string, separateGitRo
 
         if (options === null) break;
 
-
         const remoteName = basename(reference, '.git');
-
         const localName = options.name ?? remoteName;
 
         const destinationDir = (d => {
-            const p = join(d, localName);
-            return isAbsolute(p) ? p : join(destinationRoot, p);
+            return makeAbsolute(configRoot, join(d, localName));
         })(options.destination ?? data.destination ?? './');
 
         const separatedGitDir = join(separateGitRoot, remoteName);
@@ -41,14 +51,16 @@ export function parseConfig(json: string, destinationRoot: string, separateGitRo
         const tag = options.tag ?? null;
 
         config.push({
-            reference,
-            displayReference: createDisplayReference(reference),
-            tag,
+            reference: apllyVariables(reference, getVariable),
+            displayReference: createDisplayReference(apllyVariables(reference, getVariable)),
+            tag: tag ? apllyVariables(tag, getVariable) : null,
             name: localName,
-            destinationDir,
-            separatedGitDir,
+            destinationDir: apllyVariables(destinationDir, getVariable),
+            separatedGitDir: apllyVariables(separatedGitDir, getVariable),
         });
     }
+
+    console.log(config);
 
     return config;
 }
@@ -59,5 +71,57 @@ function createDisplayReference(s: string): string {
 
     return s.replace(authenticationReplacer, (_match, protocol, username, _password) => {
         return `${protocol}${username}:●●●●●@`;
+    });
+}
+
+
+function parseVariables(root: string, list: VariablesType): Map<string, string> {
+    const variables = new Map<string, string>();
+
+    for (const [name, value] of Object.entries(list)) {
+        // TODO: Check if name is valid
+
+        if (typeof value === "string") {
+            variables.set(name, value);
+        } else {
+            const path = makeAbsolute(root, value.from);
+            const content = Deno.readTextFileSync(path);
+
+            variables.set(name, content);
+        }
+    }
+
+    return variables;
+}
+
+
+function createVariableGetter(store: Map<string, string>, useEnv = true): (name: string) => string | undefined {
+    return (name: string) => {
+        if (store.has(name)) return store.get(name);
+        if (useEnv) return Deno.env.get(name);
+
+        return undefined;
+    }
+}
+
+
+function apllyVariables(s: string, getVariable: (name: string) => string | undefined): string {
+    regex.variable.lastIndex = 0;
+    return s.replace(regex.variable, (_match, _g1, _g2, _g3, _g4, _g5, groups) => {
+        const { name, filter } = groups;
+
+        const value = getVariable(name);
+
+        if (value === undefined) return `\$${name}`;
+        if (filter === undefined) return value;
+
+        switch (filter.toLowerCase()) {
+            case 'encodeURIComponent'.toLowerCase(): return encodeURIComponent(value);
+            case 'encodeURI'.toLowerCase(): return encodeURI(value);
+
+            default:
+                console.log(styles.warning(`> Unknown filter: ${filter}`));
+                return value;
+        }
     });
 }
