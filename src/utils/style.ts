d29cd65eb10f6bdb2694c61ec743c33691211df3
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-12-05 12:48:29 +0100

    Update styles

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -3,3 +3,6 @@ import { flow } from "../../libs/esm/fp-ts/function.ts";
 
 
 export const error = flow(c.brightRed, c.underline);
+
+export const progressFull = flow(c.brightGreen, c.bold);
+export const progressBlank = flow(c.gray);
