022f839ea0bf12324211a69f04e6a9e20b8ed25e
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-12-06 18:04:04 +0100

    Update color helpers

--- libs/deno_std/fmt/colors.ts
+++ libs/deno_std/fmt/colors.ts
@@ -1,1 +1,9 @@
 export * from "https://deno.land/std@0.167.0/fmt/colors.ts"
+import * as c from "https://deno.land/std@0.167.0/fmt/colors.ts"
+
+
+export const rgb8 = (color: number) => (str: string) => c.rgb8(str, color);
+export const bgRgb8 = (color: number) => (str: string) => c.bgRgb8(str, color);
+
+export const rgb24 = (color: number) => (str: string) => c.rgb24(str, color);
+export const bgRgb24 = (color: number) => (str: string) => c.bgRgb24(str, color);
--- src/utils/style.ts
+++ src/utils/style.ts
@@ -1,16 +1,12 @@
 import * as c from "../../libs/deno_std/fmt/colors.ts";
 import { flow } from "../../libs/esm/fp-ts/function.ts";
 
-
-const rgb8 = (color: number) => (str: string) => c.rgb8(str, color);
-
-
 export const syntaxError = flow(c.brightRed, c.underline);
 export const error = flow(c.red, c.bold);
 export const success = flow(c.brightGreen, c.bold);
 export const neutral = flow(c.gray);
-export const botPet = flow(rgb8(0xffffff));
-export const botMessage = flow(rgb8(0xffffff), c.italic);
+export const botPet = flow(c.rgb8(0xffffff));
+export const botMessage = flow(c.rgb8(0xffffff), c.italic);
 
 
 export const progressFull = flow(c.brightGreen, c.bold);
