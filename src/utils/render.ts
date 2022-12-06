10aa1c6246f1a9384a0ad5774ac49f4b7e4ef2f6
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-12-06 13:28:50 +0100

    Update render helper

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,1 +1,13 @@
+import * as style from "./style.ts";
+
+
 export const list = (list: string[]) => list.map(line => `• ${line}`).join('\n');
+
+export const indent = (text: string, count: number) => text.split('\n').map(line => '  '.repeat(count) + line).join('\n');
+
+export const cliErrorOutputs = (outputs: string[]) => {
+    const label = outputs.length > 1 ? 'Errors' : 'Error';
+
+    console.log(style.error(`${label}:`));
+    outputs.forEach(s => console.log(indent(s.trim() + '\n', 1)));
+}
