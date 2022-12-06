c1ffe0f696b533d525cf5293572e7e4217036b1b
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-12-06 13:28:26 +0100

    Update styles

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -2,7 +2,16 @@ import * as c from "../../libs/deno_std/fmt/colors.ts";
 import { flow } from "../../libs/esm/fp-ts/function.ts";
 
 
-export const error = flow(c.brightRed, c.underline);
+const rgb8 = (color: number) => (str: string) => c.rgb8(str, color);
+
+
+export const syntaxError = flow(c.brightRed, c.underline);
+export const error = flow(c.red, c.bold);
+export const success = flow(c.brightGreen, c.bold);
+export const neutral = flow(c.gray);
+export const botPet = flow(rgb8(0xffffff));
+export const botMessage = flow(rgb8(0xffffff), c.italic);
+
 
 export const progressFull = flow(c.brightGreen, c.bold);
 export const progressBlank = flow(c.gray);
