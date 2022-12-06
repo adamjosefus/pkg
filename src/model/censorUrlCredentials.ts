147a84093ffec668544a6da9ddc3e6f70a40ea49
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-12-06 16:45:32 +0100

    Better performance

--- src/model/censorUrlCredentials.ts
+++ src/model/censorUrlCredentials.ts
@@ -2,15 +2,16 @@
  * @author Adam Josefus
  */
 
+
+const parser = /^(?<protocol>\w+:\/\/)(?<authentication>[A-z0-9_\.\-]+\:?[A-z0-9_\.\-]*)@/i
+
+
 /**
  * Hides credentials in the URL.
  * 
  * `usr:pass@example.com` → `●●●@example.com`
  */
-export function censorUrlCredentials(url: URL | string): string {
-    const regex = /^(?<protocol>\w+:\/\/)(?<authentication>[A-z0-9_\.\-]+\:?[A-z0-9_\.\-]*)@/i
-
-    return url.toString().replace(regex, (_match, protocol, _authentication) => {
-        return `${protocol}●●●@`
-    })
+export const censorUrlCredentials = (url: URL | string): string => {
+    parser.lastIndex = 0
+    return url.toString().replace(parser, (_match, protocol, _authentication) => `${protocol}●●●@`)
 }
